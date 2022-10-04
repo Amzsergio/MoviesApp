29c0fb2b519f246ef0adb10f2549a1f36c204b6e
Amzsergio <ac.sergioandres@gmail.com>
2022-10-04 18:14:13 -0500

    footer completed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,30 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
-import './Footer.css'
 
 
 const Footer = () => {
   return (
     <div>
-        <footer className='text-white py-4 bg-dark footer'>
+        <footer className='text-white py-2 bg-dark'>
             <CardGroup className="container">
-                    <Card>
-                        <Link to='/' class='text-decoration-none text-reset fw-bold fs-4'>
-                            <img src="/yourMovieLogo.png" alt="yourMovie Logo" width='60' height='60' /> MyMovies
+                    <Card border='dark' className='bg-dark py-4'>
+                        <Link to='/' className='text-center text-decoration-none fw-bold fs-4 text-danger'>
+                            <Card.Img  style={ {width: "50px"}} variant='top' src="/yourMovieLogo.png" alt="yourMovie Logo" /> MyMovies
                         </Link>
                     </Card>
-                    <Card>
-                        <ul className='col-12 col-md-4 list-unstyled'>
-                            <li className='fw-bold mb-2 text-center'> My Movies</li>
+                    <Card border='dark' className='bg-dark text-center'>
+                        <ul className='col-12 col-md-4 list-unstyled mx-auto' >
+                            <li className='fw-bold mb-2 text-center text-danger'> My Movies</li>
                             <li><Link to='/' className='text-decoration-none text-reset text-center'>Home</Link></li>
                             <li><Link to='/about' class='text-decoration-none text-reset'>About</Link></li>
                             <li><Link to='/favourites' class='text-decoration-none text-reset'>Favourites</Link></li>
                         </ul>
                     </Card>
-                    <Card>
-                        <ul className='col-12 col-md-4 list-unstyled'>
-                            <li className='fw-bold mb-4 text-center'> Social Media</li>
+                    <Card border='dark' className='bg-dark'>
+                        <ul className='col-12 col-md-4 list-unstyled mx-auto'>
+                            <li className='fw-bold mb-4 text-center text-danger' > Social Media</li>
                             <li className='d-flex justify-content-between'>
                                 <a href="https://github.com/Amzsergio" target='_blank' class='text-decoration-none text-reset'>
                                     <i className="bi bi-github"></i>
